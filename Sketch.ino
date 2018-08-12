8fbd6597a2c856841d0b5bed9248615b8fccc82b
Lucas Soares Gomes <lucas.s.gomes@ufv.br>
2018-08-12 15:20:47 -0300

    Automação Residencial - Acendimento de Lâmpada com bate palmas

--- Sketch.ino
+++ Sketch.ino
@@ -1,51 +1,53 @@
-#define tmpMaxPalma  100 //Tempo se encontra em milisegundos
-#define tmpMaxPalmas 500 //Tempo se encontra em milisegundos
+#define tmpMaxPalma  100
+#define tmpMaxPalmas 500
 #define pinMic 7
 #define pinRelay 8
 
 unsigned long tmpEspera = 0;
 unsigned long tmpPalmas = 0;
+int sensorMic;
 int contPalmas = 0;
 void Action();
 
 void setup() {
   pinMode(pinMic, INPUT);
   pinMode(pinRelay, OUTPUT);
-  digitalWrite(pinRelay, HIGH); //Ligar arduino com Relay desligado
+  digitalWrite(pinRelay, HIGH); //Ligar arduino com Relé desligado
 }
 
 void loop() {
-  //verifica o estado do sensor de som (ele fica normalmente com a porta ligada. Quando ouver uma palma, ele desliga momentaneamente a porta)
-  int sensorMic = digitalRead(pinMic);
+  /*Normalmente o sensor mantém sua porta ligada até que uma palma seja detectada.
+  A função digitalRead() faz a leitura contínua do microfone de eletreto.
+  */
+   sensorMic = digitalRead(pinMic);
 
-  //se o sensor detectou palmas
+  //Palma detectada
   if (sensorMic == LOW) {
 
-     //espera um tempo para nao detectar a mesma palma mais de uma vez 
-     if (tmpEspera == 0) {
+     /* É necessário um tempo para que uma palma não seja detectada novamente.
+      * Incrementamos o contador a cada detecção.
+      */
+     if ((millis() - tmpEspera) >= tmpMaxPalma) {
+        tmpEspera = 0;
+     } else if (tmpEspera == 0) {
         tmpEspera = tmpPalmas = millis(); 
         contPalmas++;
-     } else if ((millis() - tmpEspera) >= tmpMaxPalma) {
-        tmpEspera = 0;
      }
   }
 
-  //caso exceda o tempo maximo entre palmas, zera o contador de palmas
-  if ( (contPalmas != 0) && ((millis() - tmpPalmas) > 500) ) {
+  //Para o tempo excedido zeramos o contador de palmas.
+  if (((millis() - tmpPalmas) > 500) && (contPalmas != 0)) {
      Action();
-     contPalmas = 0;
      tmpPalmas = millis();
+     contPalmas = 0;
   }
-  
 }
 
-
-void Action() 
-{
-  
-  switch (contPalmas) {
-    case 2: 
-       digitalWrite(pinRelay, !digitalRead(pinRelay));
-       break;
-  }
+void Action() {
+  /*
+   * Pode ser utilizado o switch no lugar do if para mais casos de uso de 3 ou mais palmas.
+   */
+   if(contPalmas == 2){
+    digitalWrite(pinRelay, !digitalRead(pinRelay));
+   }
 }
